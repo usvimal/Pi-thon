8397059055437316b7f6ccdb4217edf900f5c230
usvimal <usvimal@gmail.com>
2018-12-06 12:19:11 +0800

    Add comments on code

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Cilian = "424781702779633684"
 WY = "260781340608167938"
 
 @client.event
+# outputs in log when bot is logged in
 async def on_ready():
     print("*Hackerman voice* I'm in")
     print(client.user)
@@ -22,6 +23,7 @@ async def on_message(message):
 	if message.author == client.user:
 		return
        
+	# deletes your message and talks through the bot
 	if message.content.startswith(';bot'):
 		await client.delete_message(message)
 		msg = message.content.split(' ')
@@ -33,6 +35,7 @@ async def on_message(message):
 			return
 		return
 	
+	#function list
 	if message.content.lower().startswith(';help'):
 		embed=discord.Embed(title="⭐ Helptext⭐ ", description="this is what I can do ;D", color=0xee0000)
 		embed.add_field(name=";bot" , value="talk through me", inline=True)
@@ -40,28 +43,33 @@ async def on_message(message):
 		await client.send_message(message.channel, embed=embed)
 		return
 
-   # credits to haoshaun
+    # credits to haoshaun
+	# vote feature, will add reactions (thumbsup and thumbsdown)
 	if message.content.startswith(';vote'):
 		await client.add_reaction(message,"\U0001F44D")
 		await client.add_reaction(message,"\U0001F44E")
 		return
 
+	# sends message when libtard is mentioned
 	if 'libtard' in message.content.lower(): 
 		msg = 'libtard epic rekt 8)'.format(message)
 		await client.send_message(message.channel,msg)
 		return
 
+	# sends thinking emoji when someone says hmm
 	elif message.content.lower().startswith('hmm'):
 		msg = '\U0001F914'.format(message)
 		await client.send_message(message.channel, msg)
 		return
 
+	# sends ben shapiro photo when someone says ok this is epic
 	if 'ok this is epic' in message.content.lower() or 'okay this is epic' in message.content.lower():
 		msg = ''.format(message) 
 		await client.send_file(message.channel, 'my_image.jpg')
 		return
 
 	#credits to haoshaun
+	#link to github
 	if message.content.lower() == ';details':
 		em = discord.Embed(title='read my code!', url='https://repl.it/@VimalMuthuvel/pi-thon', colour=0xb949b5)
 		em = em.set_author(name='Minininja',url='https://github.com/usvimal')
