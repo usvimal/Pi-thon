cb6611c838b5d30209c554244dfd47b4fd38fc7f
usvimal <usvimal@gmail.com>
2019-02-02 18:57:08 +0800

    add login details

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ WY = "260781340608167938"
 @client.event
 # outputs in log when bot is logged in
 async def on_ready():
-    print("*Hackerman voice* I'm in")
-    print(client.user)
+	print("*Hackerman voice* I'm in")
+	print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
 	print('--------')
 	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
 	print('--------')
 
-    await client.change_presence(game=discord.Game(name='epic games'))
-    return
+	await client.change_presence(game=discord.Game(name='epic games'))
+	return
 			
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
