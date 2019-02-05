cd0eaac362210369466b41bbcd300e61f3736607
usvimal <usvimal@gmail.com>
2019-02-05 21:12:47 +0800

    use discord.ext stuff

--- main.py
+++ main.py
@@ -2,83 +2,36 @@ import discord
 import os
 import platform
 import asyncio
+import typing
 
-from asyncio.subprocess import PIPE
 from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=';', pm_help=None, description='A bot that does stuff.... probably')
 
 creatorID = int(os.environ.get("creatorID"))
 CillyID = int(os.environ.get("CillyID"))
 WYID = int(os.environ.get("WYID"))
+creator = os.environ.get("creator")
 
-@client.event
+
+@bot.event
 # outputs in log when bot is logged in
 async def on_ready():
 	print("*Hackerman voice* I'm in")
-	print('Logged in as ' + str(client.user.name) + ' (ID:' + str(client.user.id) + ') | Connected to ' + str(
-		len(client.guilds)) + ' servers | Connected to ' + str(len(set(client.get_all_members()))) + ' users')
+	print('Logged in as ' + str(bot.user.name) + ' (ID:' + str(bot.user.id) + ') | Connected to ' + str(
+		len(bot.guilds)) + ' servers | Connected to ' + str(len(set(bot.get_all_members()))) + ' users')
 	print('--------')
 	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__,
 																			   platform.python_version()))
-	await client.change_presence(activity=discord.Game(name='epic games'))
+	await bot.change_presence(activity=discord.Game(name='epic games'))
 	return
 
 
-@client.event
 async def on_message(message):
+
 	# we do not want the bot to reply to itself
-	if message.author == client.user:
+	if message.author == bot.user:
 		return
-
-	# deletes your message and talks through the bot
-	if message.content.startswith(';bot'):
-		await message.delete()
-		msg = message.content.split(' ')
-		msg = ' '.join(msg[1:])
-		print(message.author.id)
-		if message.author.id == creatorID or message.author.id == CillyID or message.author.id == WYID:
-			msg = msg.format(message)
-			await message.channel.send(msg)
-			return
-		else:
-			msg = ('You are not authorised sorry! {0.author.mention}').format(message)
-			await message.channel.send(msg)
-			return
-
-	# function list
-	if message.content.lower().startswith(';help'):
-		embed = discord.Embed(title="⭐ Helptext⭐ ", description="this is what I can do ;D", color=0xee0000)
-		embed.add_field(name=";bot", value="talk through me", inline=True)
-		embed.add_field(name=";details", value="view my code", inline=True)
-		embed.add_field(name=";vote", value="enter no. of seconds for the vote to carry on", inline=True)
-		await message.channel.send(embed=embed)
-		return
-	# credits to haoshaun
-	# vote feature, will add reactions (thumbsup and thumbsdown) and output final result
-	if message.content.startswith(';vote'):
-		msg = message.content.split(' ')
-		try:
-			msg = int(' '.join(msg[1:]))
-		except ValueError:
-			await message.channel.send(
-				"Please structure the message such that ;vote <duration of voting in seconds>")
-		else:
-			await message.add_reaction('✅')
-			await message.add_reaction('❌')
-		print(msg)
-		await asyncio.sleep(msg)
-		reactions = (await message.channel.get_message(message.id)).reactions
-		print(reactions)
-		counts = {}
-		for reaction in reactions:
-			counts[reaction.emoji] = reaction.count - 1
-			print(f'{reaction.emoji} = {counts}')
-		if counts['✅'] > counts['❌']:
-			await message.channel.send("The results are out: '✅'")
-		else:
-			await message.channel.send("The results are out: '❌'")
-			return
 
 	# sends message when libtard is mentioned
 	if 'libtard' in message.content.lower():
@@ -94,26 +47,59 @@ async def on_message(message):
 
 	# sends ben shapiro photo when someone says ok this is epic
 	if 'ok this is epic' in message.content.lower() or 'okay this is epic' in message.content.lower():
-		msg = ''.format(message)
 		await message.channel.send(file=discord.File('my_image.jpg'))
 		return
 
-	#credits to haoshaun
-	#link to github
-	if message.content.lower() == ';details':
-		em = discord.Embed(title='read my code!', url='https://github.com/usvimal/Pi-thon', colour=0xb949b5)
-		em=em.set_author(name='Minininja',url='https://github.com/usvimal')
-		await message.channel.send(embed=em)
-		return
-
-	#sends me a message if I am mentioned
-	if 'vimal' in message.content.lower():
+	# sends me a message if I am mentioned
+	if creator in message.content.lower():
 		msg = message.content.lower().format(message)
 		author = message.author
-		em = discord.Embed(title='Someone messaged you!', description= msg, colour=0xFF00FF)
-		em.set_author(name= author,icon_url=author.avatar_url)
-		channel = client.get_user(creatorID)
+		em = discord.Embed(title='Someone messaged you!', description=msg, colour=0xFF00FF)
+		em.set_author(name=author, icon_url=author.avatar_url)
+		channel = bot.get_user(creatorID)
 		await channel.send(embed=em)
 
+
+@bot.command()
+async def bot(ctx, *, arg):
+	"""deletes your message and talks through the bot"""
+	await ctx.delete(arg)
+	print(arg.author.id)
+	if arg.author.id == creatorID or arg.author.id == CillyID or arg.author.id == WYID:
+		await ctx.send(arg)
+		return
+	else:
+		await ctx.send('You are not authorised sorry! {0.author.mention}')
+		return
+
+
+@bot.command()
+async def vote(ctx, time: int, *, reason: str):
+	"""vote feature, will add reactions (thumbsup and thumbsdown) and output final result"""
+	"""enter time in seconds and your reason"""
+	await ctx.add_reaction('✅')
+	await ctx.add_reaction('❌')
+	await asyncio.sleep(time)
+	reactions = (await ctx.get_message(ctx.id)).reactions
+	print(reactions)
+	counts = {}
+	for reaction in reactions:
+		counts[reaction.emoji] = reaction.count - 1
+		print(f'{reaction.emoji} = {counts}')
+	if counts['✅'] > counts['❌']:
+		await ctx.send("The answer to" + reason + ": ✅")
+	else:
+		await ctx.send("The answer to" + reason + ": ❌")
+		return
+
+
+async def details(ctx):
+	"""link to github"""
+	em = discord.Embed(title='read my code!', url='https://github.com/usvimal/Pi-thon', colour=0xb949b5)
+	em = em.set_author(name='Minininja', url='https://github.com/usvimal')
+	await ctx.send(embed=em)
+	return
+
+
 token = os.environ.get("DISCORD_BOT_SECRET")
-client.run(token)
+bot.run(token)
