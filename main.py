b4e4728069e07dcc4059dbfb9ec621aa8620a913
usvimal <usvimal@gmail.com>
2019-04-15 07:04:18 +0800

    Import urllib

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 from discord.ext import commands
 import owotrans
 import lyricsgenius
+from urllib.error import HTTPError
 
 bot = commands.Bot(command_prefix=';', pm_help=None, description='A personal project for fun')
 
@@ -170,7 +171,7 @@ async def lyrics(ctx):
 		em = em.set_author(name='Genius')
 		async with ctx.typing():
 			await ctx.send(embed=em)
-	except HTTPException:
+	except Error:
 		await ctx.send(song_title)
 		await ctx.send(lyrics)	
 	else:
