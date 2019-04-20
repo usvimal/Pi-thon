8236b84b07c92de1a5e1f2b3362a973569e7b69d
usvimal <usvimal@gmail.com>
2019-04-20 12:45:30 +0800

    fix formatting

--- config/games.py
+++ config/games.py
@@ -1,7 +1,7 @@
 import discord
 
 games = [
-	(discord.ActivityType.playing, '| Connected to ' + guilds + ' servers | Connected to ' + members + ' users'),
+	(discord.ActivityType.playing, '| Connected to {guilds} servers | Connected to {members} users'),
 	(discord.ActivityType.playing, 'epic games'),
 	(discord.ActivityType.watching, 'paint dry..'),
 	(discord.ActivityType.watching, 'you right now'),
