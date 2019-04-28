ec3ad14536739f84d9c4aaa2a18cc0fcb7954e4d
mk43v3r <mk43v3r@gmail.com>
2019-04-28 16:52:43 +0800

    2nd update to lyrics.py by Min
    - Added self.user_context_dict which pairs the user with the context which ";lyrics start" was called in
    - Added stop() function and ";lyrics stop" command which will deregister the user if the user does not want to receive any more lyrics.
    - Added @commands.Cog.listener() decorator to on_member_update
    - on_member_update will only display lyrics for those who registered using ";lyrics start"

--- cogs/lyrics.py
+++ cogs/lyrics.py
@@ -11,11 +11,13 @@ def chunks(s, n):
 		yield s[start:start + n]
 
 
-class Lyrics(commands.Cog):
+	class Lyrics(commands.Cog):
 	def __init__(self, bot):
+		""" Registers genius token and add a list of registered user and their context"""
 		self.bot = bot
 		genius_token = os.environ.get("genius_token")
 		self.genius = lyricsgenius.Genius(genius_token)
+		self.user_context_dict = dict()
 
 	@commands.group()
 	async def lyrics(self, ctx):
@@ -25,17 +27,32 @@ class Lyrics(commands.Cog):
 
 	@lyrics.command()
 	async def start(self, ctx):
-		song_title, song_artist = self.get_song_description(ctx.message.author.activity)
-		await self.show_lyrics_from_description(ctx, song_title, song_artist)
+		""" Register the user to the user-context dictionary and show the first song"""
+		if ctx.author not in self.user_context_dict:
+			self.user_context_dict[ctx.author] = ctx
+			song_title, song_artist = self.get_song_description(ctx.message.author.activity)
+			await self.show_lyrics_from_description(ctx, song_title, song_artist)
 
+	@lyrics.command()
+	async def stop(self, ctx):
+		""" Deregister the user from the dictionary """
+		if ctx.author in self.user_context_dict:
+			del self.user_context_dict[ctx.author]
+
+	@commands.Cog.listener()
 	async def on_member_update(self, before, after):
-			if after.activity == "None":
-				return None
-			else:
-					before_description = self.get_song_description(before.activity)
-					after_description = self.get_song_description(after.activity)
-					if before_description != after_description:
-							await self.show_lyrics_from_description(*after_description)
+		""" If the user is registered and the next activity is still Spotify, show new lyrics. """
+		if before in self.user_context_dict and after.activity != "None":
+				# Get the context of registered user and update the dictionary
+				ctx = self.user_context_dict[before]
+				del self.user_context_dict[before]
+				self.user_context_dict[after] = ctx
+
+				before_description = self.get_song_description(before.activity)
+				after_description = self.get_song_description(after.activity)
+				if before_description != after_description:
+						await self.show_lyrics_from_description(ctx, *after_description)
+
 
 	def get_song_description(self, activity):
 		""" Get the description of a song from user activity. """
@@ -50,7 +67,7 @@ class Lyrics(commands.Cog):
 		for chunk in chunks(self.get_lyrics(song_title, song_artist), 2048):
 			try:
 				em = discord.Embed(title=song_title, description=chunk)
-				em = em.set_author(name='Genius')
+				em = em.set_author(name=song_artist)
 				async with ctx.typing():
 					await ctx.send(embed=em)
 			except AttributeError:
