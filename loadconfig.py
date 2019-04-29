a5c10344f1e32e68473bba569171799f6541fe79
usvimal <usvimal@gmail.com>
2019-04-29 17:45:10 +0800

    Add min

--- loadconfig.py
+++ loadconfig.py
@@ -1,3 +1,3 @@
 from config.games import games, gamestimer
-from config.environment import creatorID, CillyID, WYID, creator, DATABASE_URL, discord_key
+from config.environment import creatorID, CillyID, WYID, creator, DATABASE_URL, discord_key, MinID
 from config.cogs import cogs
