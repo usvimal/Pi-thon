94f97ace3851899f5cd498b2817cb8358f7a906d
usvimal <usvimal@gmail.com>
2019-05-01 17:45:11 +0800

    implement on reaction add instead

--- cogs/to-do.py
+++ cogs/to-do.py
@@ -12,19 +12,23 @@ class Todo(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.channelid = 572561960982413334
+		self.message = bot.message
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
 		if message.channel.id == self.channelid:
 			await message.add_reaction('✅')
-			reactions = (await message.channel.fetch_message(message.id)).reactions
-			counts = {}
-			for reaction in reactions:
-				counts[reaction.emoji] = reaction.count - 1
-				print(f'{reaction.emoji} = {counts}')
-			if counts['✅'] > 0:
-				await message.delete()
-				await message.channel.send(strike(message.content))
+			await message.add_reaction('❌')
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self, reaction):
+		if self.message.channel.id == self.channelid:
+			if reaction == '✅':
+				await self.message.delete()
+				await self.message.channel.send(strike(self.message.content))
+			if reaction == '❌':
+				await self.message.delete()
+
 
 
 def setup(bot):
