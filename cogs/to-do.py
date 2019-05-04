097b7d94ac3b3561990e0307f03038b0c26a8aa2
usvimal <usvimal@gmail.com>
2019-05-04 13:21:15 +0800

    fixes and improvements to to-do
    -convert emoji to string before verifying
    -add checks for bot user

--- cogs/to-do.py
+++ cogs/to-do.py
@@ -11,11 +11,14 @@ def strike(text):
 class Todo(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.channelid = 572561960982413334
+		self.todo_channel_id = 572561960982413334
 
 	@commands.Cog.listener()
 	async def on_message(self, message):
-		if message.channel.id == self.channelid:
+		if message.author == self.bot.user:
+			return
+
+		if message.channel.id == self.todo_channel_id:
 			await message.add_reaction('✅')
 			await message.add_reaction('❌')
 
@@ -23,12 +26,17 @@ class Todo(commands.Cog):
 	async def on_raw_reaction_add(self, payload):
 		channel = self.bot.get_channel(payload.channel_id)
 		message = await channel.fetch_message(payload.message_id)
+		emoji = payload.emoji
+		user = message.guild.get_member(payload.user_id)
 
-		if payload.channel_id == self.channelid:
-			if payload.emoji == '✅':
+		if user == self.bot.user:
+			return
+
+		if payload.channel_id == self.todo_channel_id:
+			if str(emoji) == '✅':
 				await message.delete()
 				await channel.send(strike(message.content))
-			if payload.emoji == '❌':
+			if str(emoji) == '❌':
 				await message.delete()
 
 
