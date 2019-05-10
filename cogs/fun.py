86118e952b0c74be505c4de22a7e2b69dd24e422
usvimal <usvimal@gmail.com>
2019-05-10 22:48:43 +0800

    Revert "Add execute command to execute python code"
    This reverts commit 4727a98d6f41645985289d35c8cb36e8e76177d5.

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,14 +12,6 @@ class Fun(commands.Cog):
 		msg = owotrans.owo(arg)
 		await ctx.send(msg)
 
-	@commands.command()
-	async def execute(self, ctx, *, arg):
-		"""Enter Python code to execute it"""
-		try:
-			exec(arg)
-		except Exception as e:
-			await ctx.send('Something went wrong... Please check your code. \n Output:' + str(e))
-
 
 def setup(bot):
 	bot.add_cog(Fun(bot))
