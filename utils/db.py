a1265cc960e4fa4b9bd89e1bb37e8fa512ed3f2e
usvimal <usvimal@gmail.com>
2019-05-12 17:12:39 +0800

    use dict

--- utils/db.py
+++ utils/db.py
@@ -7,8 +7,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 def open_sheet():
 	scope = ['https://spreadsheets.google.com/feeds',
 	         'https://www.googleapis.com/auth/drive']
-	cred = str(config.GCP_CRED)
+	cred = config.GCP_CRED
 	json_obj = json.loads(cred)
-	credentials = ServiceAccountCredentials.from_json_keyfile_name(json_obj, scope)
+	credentials = ServiceAccountCredentials.from_json_keyfile_dict(json_obj, scope)
 	gc = gspread.authorize(credentials)
 	wks = gc.open("pi-thon database").sheet1
