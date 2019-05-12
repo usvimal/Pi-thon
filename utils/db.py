474471f8cee1221dd6bcc48c9606b2722664ce18
usvimal <usvimal@gmail.com>
2019-05-12 16:58:00 +0800

    fix derp

--- utils/db.py
+++ utils/db.py
@@ -8,7 +8,7 @@ def open_sheet():
 	scope = ['https://spreadsheets.google.com/feeds',
 	         'https://www.googleapis.com/auth/drive']
 	cred = config.GCP_CRED
-	json_obj = json.loads(cred.read())
+	json_obj = json.loads(cred)
 	credentials = ServiceAccountCredentials.from_json_keyfile_name(json_obj, scope)
 	gc = gspread.authorize(credentials)
 	wks = gc.open("pi-thon database").sheet1
