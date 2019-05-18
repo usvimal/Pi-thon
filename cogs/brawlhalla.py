3f28cf287b79066b94882ea0761a85974cdf5f71
usvimal <usvimal@gmail.com>
2019-05-18 15:29:11 +0800

    fixes to Brawlhalla cog
    -use variable self.up to indicate if the site is actually up

--- cogs/brawlhalla.py
+++ cogs/brawlhalla.py
@@ -8,6 +8,7 @@ from discord.ext import commands
 class Brawlhalla(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.up = ''
 
 	def ping(self, host):
 		"""
@@ -21,22 +22,25 @@ class Brawlhalla(commands.Cog):
 		# Building the command. Ex: "ping -c 1 google.com"
 		command = ['ping', param, '1', host]
 
-		return subprocess.run(command) == 0
+		if subprocess.call(command) == 0:
+			self.up = True
+		else:
+			self.up = False
 
 	@commands.Cog.listener()
 	async def on_member_update(self, before, after):
 		try:
 			if after.activity.name == 'Brawlhalla' and self.bot.brawlhalla_status.get(before.id, default=False) is True:
-				pong = self.ping('pingtest-sgp.brawlhalla.com')
-				if pong:
+				self.ping('pingtest-sgp.brawlhalla.com')
+				if not self.up:
 					channel = self.bot.get_user(after.id)
 					await channel.send('Oof! Brawlhalla is down! I will let you know when its back up')
 
 					async def check():
-						while pong:
+						while not self.up:
 							self.ping('pingtest-sgp.brawlhalla.com')
 							await asyncio.sleep(60 * 5)
-						await channel.send('yay its back up!')
+						await channel.send('yay Brawlhalla is back up! Go play :)')
 
 					await check()
 		except:
