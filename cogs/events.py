e63f611f60aa3918510ab7548b08b812dda1dd6f
usvimal <usvimal@gmail.com>
2019-05-25 17:28:02 +0800

    ignore if command has local error handler

--- cogs/events.py
+++ cogs/events.py
@@ -17,6 +17,9 @@ class Events(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_command_error(self, ctx, error):
+		if hasattr(ctx.command, 'on_error'):
+			return
+
 		if isinstance(error, commands.MissingRequiredArgument):
 			return await ctx.send(f"Hol\' up you forgot an argument: {error.param.name}")
 
@@ -72,7 +75,7 @@ class Events(commands.Cog):
 async def on_guild_join(self, guild):
 	default_prefix = config.default_prefix
 	self.bot.all_prefixes[guild.id] = default_prefix
-	async with self.bot.db.acquire() as db:
+	async with self.bot.dbpool.acquire() as db:
 		await db.execute(						'UPDATE guildprop SET "prefix"=$1 WHERE "guild_id"=$2;',
 						default_prefix, guild.id)
 
@@ -80,7 +83,7 @@ async def on_guild_join(self, guild):
 @commands.Cog.listener()
 async def on_guild_remove(self, guild):
 	del self.bot.prefixes[guild.id]
-	async with self.bot.db.acquire() as db:
+	async with self.bot.dbpool.acquire() as db:
 		await db.execute("DELETE FROM guildprop WHERE guild=$1", guild.id)
 
 
