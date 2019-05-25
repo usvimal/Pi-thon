49633bee0a162f91bcb83ae30088bb8a984a3574
usvimal <usvimal@gmail.com>
2019-05-25 21:28:15 +0800

    tasks are now embeds

--- cogs/to-do.py
+++ cogs/to-do.py
@@ -1,3 +1,5 @@
+import discord
+
 from discord.ext import commands
 from utils import checks
 from utils.text_formatter import strike
@@ -10,13 +12,16 @@ class Todo(commands.Cog):
 
 	@checks.is_officer()
 	@commands.command()
-	async def task(self, ctx):
+	async def task(self, ctx, *, arg):
 		"""Add task to todo channel"""
 		channel = self.bot.get_channel(self.todo_channel_id)
-		task = await channel.send(ctx.message.content)
+		em = discord.Embed(title=arg)
+		em.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+		task = await channel.send(embed=em)
 		await task.add_reaction('✅')
 		await task.add_reaction('❌')
 
+	@checks.in_hideout_task_channel()
 	@commands.Cog.listener()
 	async def on_raw_reaction_add(self, payload):
 		channel = self.bot.get_channel(payload.channel_id)
@@ -24,13 +29,23 @@ class Todo(commands.Cog):
 		emoji = payload.emoji
 		user = message.guild.get_member(payload.user_id)
 
-		if user == self.bot.user:
-			return
-
-		if payload.channel_id == self.todo_channel_id:
+		try:
+			embed = message.embeds[0]
+		except:
 			if str(emoji) == '✅':
 				await message.delete()
 				await channel.send(strike(message.content))
+			if str(emoji) == '❌':
+				await message.delete()
+		else:
+			if str(emoji) == '✅':
+				striked_message = strike(embed['title'])
+				author_image = embed['thumbnail']['url']
+				author = embed['author']
+				await message.delete()
+				em = discord.Embed(title=striked_message)
+				em.set_author(name=author,icon_url=author_image)
+				channel.send(embed=em)
 			if str(emoji) == '❌':
 				await message.delete()
 
--- utils/checks.py
+++ utils/checks.py
@@ -6,3 +6,9 @@ def is_officer():
     def predicate(ctx):
         return ctx.message.author.id == config.MinID or config.creatorID
     return commands.check(predicate)
+
+
+def in_hideout_task_channel():
+    async def predicate(ctx):
+        return ctx.guild.id == 572561960982413334
+    return commands.check(predicate)
