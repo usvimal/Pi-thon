c569fcd4ff323686fe8dad44709913a93d3aa440
usvimal <usvimal@gmail.com>
2019-06-16 00:46:18 +0800

    automatically delete the deletion message after 10 seconds

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from discord.ext import commands
 
 
@@ -9,8 +11,10 @@ class Moderation(commands.Cog):
 	@commands.has_permissions(manage_messages=True)
 	@commands.command(aliases=["purge"])
 	async def clean(self, ctx, *, limit: int):
-		await ctx.purge(limit=limit + 1)  # also deletes your own message
-		await ctx.send(f"`{limit}` messages were deleted")
+		await ctx.message.channel.purge(limit=limit + 1)  # also deletes your own message
+		deletion_message = await ctx.send(f"`{limit}` messages were deleted")
+		await asyncio.sleep(10)
+		await deletion_message.delete()
 
 
 def setup(bot):
