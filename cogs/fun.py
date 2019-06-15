702a8652eb1e7370bcc214c1f3dbcdfac2724c68
usvimal <usvimal@gmail.com>
2019-06-15 23:28:40 +0800

    move the ;talk command into a check so that it doesnt appear in the help command for others

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,8 @@
-import owotrans
-from discord.ext import commands
 import config
+import owotrans
+
+from discord.ext import commands
+from utils.checks import is_approved_talker
 
 
 class Fun(commands.Cog):
@@ -13,18 +15,13 @@ class Fun(commands.Cog):
 		msg = owotrans.owo(arg)
 		await ctx.send(msg)
 
-	@commands.command()
+	@commands.command(aliases=["say"])
+	@is_approved_talker()
 	@commands.bot_has_permissions(manage_messages=True)
 	async def talk(self, ctx, *, arg):
 		"""deletes your message and talks through the bot"""
 		await ctx.message.delete()
-		print(ctx.message.author.id)
-		if ctx.message.author.id == config.creatorID or ctx.message.author.id == config.CillyID or ctx.message.author.id == config.WYID or ctx.message.author.id == config.MinID:
-			await ctx.send(arg)
-			return
-		else:
-			await ctx.send(f'You are not authorised sorry! {ctx.author.mention}')
-			return
+		await ctx.send(arg)
 
 
 def setup(bot):
--- utils/checks.py
+++ utils/checks.py
@@ -1,1 +1,8 @@
-#for future checks
+import config
+from discord.ext import commands
+
+def is_approved_talker():
+	def predicate(ctx):
+		author_id = ctx.message.author.id
+		return author_id == config.creatorID or author_id == config.CillyID or author_id == config.WYID or author_id == config.MinID
+	return commands.check(predicate)
